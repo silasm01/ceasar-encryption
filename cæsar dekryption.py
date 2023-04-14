964633e625089c7b36c82fcb3e41f9d8a9cd163c
silasm01 <Silasmikkelsen@outlook.com>
2023-04-14 11:56:31 +0200

    Finished

--- cæsar dekryption.py
+++ cæsar dekryption.py
@@ -8,41 +8,79 @@ def closest(lst, K):
     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
 def frek_analyse(txt, alfabet):
-    temp = []
-    for i in range(0, 25):
+    # Laver en liste som skal bruges til at gemme de forskelige keys med optællinger af bogstaverne i dem
+    score = []
+    # Går igennem alle keys der er op til længden af alfabetet
+    for key in range(0, len(alfabet)):
+        # Laver en dictionary som bruges til at optælle mængden af hvert tegn
         optælling = {bogstav: 0 for bogstav in alfabet}
-        for k in txt:
-            if k in optælling:
-                o = alfabet.index(k)+i if alfabet.index(k)+i < len(alfabet) else alfabet.index(k)+i-len(alfabet)
-                optælling[alfabet[o]] += 1
+        # Går igennem alle tegn i teksten
+        for character in txt:
+            if character in optælling:
+                # Sætter variablen tegn til tegnets index adderet med keyen. Den tjekker også om tegnet er uden for alfabetet og minusser med alfabetes længde.
+                tegn = alfabet.index(character)+key if alfabet.index(character)+key < len(alfabet) else alfabet.index(character)+key-len(alfabet)
+                optælling[alfabet[tegn]] += 1
 
-        for f in freq:
-            if optælling[f] != 0:
+        # Går igennem de 5 mest brugte bogstaver i dansk sprog.
+        for char_freq in freq:
+            if optælling[char_freq] != 0:
+                # Optæller hvor mange gange hver af de 5 bogstaver bliver brugt
                 try:
-                    temp[i] += freq[f]-len(txt)/optælling[f]
+                    score[key] += freq[char_freq]-len(txt)/optælling[char_freq]
                 except:
-                    temp.append(freq[f]-len(txt)/optælling[f])
-        print(f'\nKey {i}: {temp[i]}')
+                    score.append(freq[char_freq]-len(txt)/optælling[char_freq])
+        print(f'\nKey {key}: {score[key]}')
 
-    print(f"Keyen er med stor sandstynlighed: \033[32m {len(alfabet)- temp.index(closest(temp, sum(freq.values())))}→({temp.index(closest(temp, sum(freq.values())))}←)")
+    return score.index(closest(score, sum(freq.values())))
 
-    print("\033[00m")
+def dekrypter(txt, alfabet, key):
+    # Går igennem alle tegn i teksten
+    for character in txt:
+        # Checker om tegnet er en del af det brugerspecifirede alfabet.
+        if character in alfabet:
+            # Checker om tegnet rykket tilbage med keyen er under 0 og hvis den er skal den addere længden af alfabetet på.
+            if alfabet.index(character)-(len(alfabet)-1-key) > 0:
+                print(alfabet[alfabet.index(character)-(len(alfabet)-key)], end ="")
+            else:
+                print(alfabet[alfabet.index(character)-(len(alfabet)-key)+len(alfabet)], end ="")
+        else:
+            # Printer alle tegn som ikke er en del af alfabet uden at tilføje en key
+            print(character, end ="")
 
 def menu():
+    # Tager input fra brugeren
     match input("Hvordan vil du indsætte teksten? \n1. Copy Paste (Skal være i en linje)\n2. Fra Fil"):
         case "1":
+            # Gemmer inputet fra brugeren som tekst.
             txt = input("Ok, paste teksen her: ")
         case "2":
-            fil_path = input("Hvor ligger filen? (Default: tekst.txt): ")
-            fil_path = "tekst.txt" if fil_path == "" else fil_path
-            with open(fil_path, 'r') as file:
-                txt = file.read()
+            # Tager input fra brugeren om lokationen af filen.
+            file_path = input("Hvor ligger filen? (Default: tekst.txt): ")
+            # Sætter file_path til default værdien hvis brugeren ikke giver et input
+            file_path = "tekst.txt" if file_path == "" else file_path
+            # Gemmer filens indhold som tekst
+            try:
+                with open(file_path, encoding="utf-8") as file: #TODO ERROR HANDLING
+                    txt = file.read()
+            except:
+                print("Filen kunne ikke åbnes prøv igen")
+                menu()
         case _:
+            # Kører menu() forfra hvis brugeren ikke giver et gyldigt input.
             print("Ugyldigt valg")
             menu()
 
-    alf = input(f"Hvilket alfabet vil du bruge? (Default: {string.ascii_uppercase}): ")
-    alf = string.ascii_uppercase if alf == "" else alf
-    frek_analyse(txt, alf)
+    # Tager input fra brugeren om alfabetet de vil bruge
+    alfabet = input(f"Hvilket alfabet vil du bruge? (Default: {string.ascii_uppercase}): ")
+    # Sætter alfabet til default alfabet hvis brugeren ikke giver et input
+    alfabet = string.ascii_uppercase if alfabet == "" else alfabet
+    # Kører frekvens analysen hvor teksten of alfabetet er sendt med
+    key = frek_analyse(txt, alfabet)
 
+    # Dekrypterer teksten hvis brugeren vil
+    match input(f"Keyen er med stor sandstynlighed: \033[32m {len(alfabet)- key}→({key}←) \033[00m\nVil du dekryptere teksten? (Y/n) "):
+        case "n":
+            return
+        case _:
+            print(dekrypter(txt, alfabet, key))
 menu()
